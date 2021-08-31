0901b7f8ebb6aecb091a5a6e07a672fd9d7cae7a
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-08-31 21:13:08 +0200

    integrated API dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
-  function handleSubmit(event) {
+  function search(event) {
     event.preventDefault();
     alert(`Searched for ${keyword}`);
+
+    //documentation: https://dictionaryapi.dev
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function saveKeyword(event) {
     setKeyword(event.target.value);
   }
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   return (
     <div className="Dictionay">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={search}>
         <input type="search" onChange={saveKeyword}></input>
       </form>
     </div>
